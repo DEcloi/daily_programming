40d670caa44ceb188bd91e9134dc2fd6e728056e
swkim <swkim0512@gmail.com>
2020-04-19 19:33:40 +0900

    programmers
    programmers - sort.01

---
+++ programmers/sort/01.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    return list(map(lambda x:sorted(array[x[0] - 1:x[1]])[x[2] - 1], commands))
+
+
+Array = [1, 5, 2, 6, 3, 7, 4]
+Commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+answer = solution(Array, Commands)
+print(answer)
